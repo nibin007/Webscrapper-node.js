801ec3d2fdc6f9ebaebda17adc2ae214a254e996
nibin007 <89572613+nibin007@users.noreply.github.com>
2022-02-12 11:27:29 +0530

    Update connection.js

--- connection/connection.js
+++ connection/connection.js
@@ -2,7 +2,7 @@ const mongoclient=require('mongodb').MongoClient
 const state={db:null}
 module.exports.connect=function(done){
     const dbname='wordcounter'
-    const url= 'mongodb+srv://nibin:arsenewenger12@cluster0.5w7yg.mongodb.net/wordcounter'
+    const url= 'mongodb+srv://nibin:@cluster0.5w7yg.mongodb.net/wordcounter'
     mongoclient.connect(url,(err,data)=>{
         if(err) return done(err)
         state.db=data.db(dbname)
